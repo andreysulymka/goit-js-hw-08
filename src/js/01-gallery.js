76a021f3316d7b3e06f10bf65d78aba54f2e4e73
andreysulymka <andreysulymka5@gmail.com>
2023-01-13 13:14:44 +0200

    add 1 part homework

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryItemMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createGalleryItemMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+      `;
+    })
+    .join('');
+};
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+let onClickModalClose;
+
+function onGalleryContainerClick(event) {
+    event.preventDefault();
+    const isGalleryRef = event.target.classList.contains('gallery__image');
+    if (!isGalleryRef) {
+        return;
+    };
+};
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
 console.log(galleryItems);
