967cd8d53a0d06a062bf898b44727901b738c435
andreysulymka <andreysulymka5@gmail.com>
2023-01-15 17:50:46 +0200

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
 
+// const iframe = document.querySelector('iframe');
+
+// iframe.on('timeupdate', onPlay);
+// const onPlay = function (data) {
+   
+// };
+// const dataJson = JSON.stringify(data);
+// localStorage.setItem("videoplayer-current-time", dataJson);
+// const time = +localStorage.getItem(VIDEO_CURRENT_TIME);
+//  player.setCurrentTime(time);
+player.on("timeupdate", throttle(function (data) {
+     const time = data.seconds;
+     console.log(time);
+    localStorage.setItem(TIME_DATA_PLAYER, time)
+},1000))
