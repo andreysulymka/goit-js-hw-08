4cbe4da1d4117e44945eb492dbf237faa0e6a5b5
andreysulymka <andreysulymka5@gmail.com>
2023-01-16 18:39:30 +0200

    add changers

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,9 @@
 import player from '@vimeo/player';
-import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';;
+import throttle from 'lodash.throttle';
 
+const iframePlayer = new Player (iframe);
 
+const iframe = document.getElementById('vimeo-player');
 
 player.on("timeupdate", throttle(function (data) {
     const time = data.seconds;
